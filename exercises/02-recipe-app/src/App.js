5b70cfc6ddf08d478c0ad199202380aee61701f9
enrikevee <everduzco@ENRIQUEs-MBP.localdomain>
2019-01-28 16:53:58 -0800

    added bootstrap navbar

--- exercises/02-recipe-app/src/App.js
+++ exercises/02-recipe-app/src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router,
+  Route,
+  Link }
+  from "react-router-dom"
+
+import Homepage from "./pages/Homepage"
+import Recipes from "./pages/Recipes"
+import NavigationBar from "./Navigationbar"
+
+
 
 class App extends Component {
   render() {
     return (
+      <div>
+        < NavigationBar/>
+      </div>
     )
   }
 }
+
 
 export default App
---
+++ exercises/02-recipe-app/src/Navigationbar.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Button, FormGroup, FormControl } from "react-bootstrap"
+
+import Homepage from "./pages/Homepage"
+import Recipes from "./pages/Recipes"
+
+
+class NavigationBar extends Component {
+  render() {
+    return (
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#home">Team Tiger Cookery</a>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Nav>
+          <NavItem eventKey={1} href="#">
+            Link
+          </NavItem>
+          <NavItem eventKey={2} href="#">
+            Link
+          </NavItem>
+          <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+            <MenuItem eventKey={3.1}>Action</MenuItem>
+            <MenuItem eventKey={3.2}>Another action</MenuItem>
+            <MenuItem eventKey={3.3}>Something else here</MenuItem>
+            <MenuItem divider />
+            <MenuItem eventKey={3.4}>Separated link</MenuItem>
+          </NavDropdown>
+          <Navbar.Form pullLeft>
+     <FormGroup>
+       <FormControl type="text" placeholder="Search" />
+     </FormGroup>{' '}
+     <Button type="submit">Submit</Button>
+   </Navbar.Form>
+        </Nav>
+      </Navbar>
+    )
+  }
+}
+
+
+export default NavigationBar
